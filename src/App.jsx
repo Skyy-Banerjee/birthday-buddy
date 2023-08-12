2c37233f1bd66ef557ed1b4cc92dd511bf2f98b2
Soumadip Banerjee <39thdecember@gmail.com>
2023-08-12 12:37:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import data from './data'
+import PersonList from './PersonList';
+
+const App = () => {
+
+  const [people, setPeople] = useState(data)
+  
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today!</h3>
+        <PersonList people={people}/>
+        <button className="btn btn-block" type='button' onClick={()=>{setPeople([])}}>clear all</button>
+      </section>
+    </main>
+  )
+};
+export default App;
---
+++ src/PersonList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Person from './Person';
+
+function PersonList({ people }) {
+	return (
+		<section>
+			{people.map((person) => {
+				return <Person key={person.id} {...person} />;
+			})}
+		</section>
+	);
+}
+
+export default PersonList;
